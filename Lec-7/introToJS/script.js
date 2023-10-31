0ada0565c1f6185ba12d292a50616db1b26a4ccc
Shreyance Bodh <shreyancebodh2013@gmail.com>
2023-10-31 12:26:21 +0530

    Intro to JS

---
+++ Lec-7/introToJS/script.js
@@ -0,0 +1,23 @@
+console.log('Hello World!!')
+
+
+// ----------- number
+a = 10;
+var b = 20;
+
+// console.log(typeof a) -->>> number
+
+var str = 'This is some string';
+var str2 = "a";
+console.log(str);
+console.log(typeof str)
+console.log(typeof str2)
+
+str2 = 20;
+console.log(str2);
+
+var isValid = !true;
+console.log(isValid);
+console.log(typeof isValid);
+
+
---
+++ Lec-7/introToJS/script2.js
@@ -0,0 +1,16 @@
+
+
+var nums = [1,2,3,4,5];
+console.log(nums);
+
+
+var arr = [1, true, 'Hello', ['a', 'b','c']];
+console.log(arr[1]);
+console.log(arr[3][1]);
+
+arr[2] = 'Hi'
+console.log(arr);
+
+for(var i=0; i<arr.length; i++){
+    console.log(arr[i]);
+}
